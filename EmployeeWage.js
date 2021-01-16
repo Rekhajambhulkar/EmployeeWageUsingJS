5f6c857dd57ffc808e30bb6c7f1938d39231af64
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-01-16 14:39:44 +0530

    Calculate Employee Wage using Function

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -20,7 +20,6 @@ function getWorkingHours(empCheck) {
                 return 0;
     }
 }
-
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
 while(totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
@@ -28,6 +27,5 @@ while(totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalEmpHrs += getWorkingHours(empCheck);
 }
-
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
 console.log("Total Days: " + totalWorkingDays + " Total Hour: " + totalEmpHrs + " Employee Wage: " + empWage );
