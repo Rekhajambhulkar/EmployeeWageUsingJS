6f53ae6e2531df1931e6a3d23ce95e4be6c445e8
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2021-01-16 14:27:24 +0530

    Calculate Employee Wage till condition of total working hrs or days reached for a month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -6,24 +6,28 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
 
 function getWorkingHours(empCheck) {
     switch(empCheck){
         case 1:
-            empHrs = PART_TIME_HOURS;
+            return PART_TIME_HOURS;
             break;
         case 2:
-            empHrs = FULL_TIME_HOURS;
+            return FULL_TIME_HOURS;
             break;
             default:
-                empHrs = 0;
+                return 0;
     }
 }
 
 let totalEmpHrs = 0;
-for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+let totalWorkingDays = 0;
+while(totalWorkingDays <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){
+    totalWorkingDays++;
     let empCheck = Math.floor(Math.random() * 10) % 3;
     totalEmpHrs += getWorkingHours(empCheck);
 }
+
 let empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("Hour: " + totalEmpHrs + " Employee Wage: " + empWage );
+console.log("Total Days: " + totalWorkingDays + " Total Hour: " + totalEmpHrs + " Employee Wage: " + empWage );
